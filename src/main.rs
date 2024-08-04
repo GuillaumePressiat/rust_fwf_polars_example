204e48e9381b51e281258136a30f044ad5228e06
GuillaumePressiat <guillaume.pressiat@gmail.com>
2024-08-04 13:43:47 +0200

    Test with polars extract groups feature

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 pub fn main() {
     use polars::prelude::*;
 
-    let mut _vec_expr = [
-        col("l")
-            .str()
-            .slice(lit(0), lit(9))
-            .cast(DataType::Int32)
-            .alias("col_1"),
-        // (col("l").str().slice(lit(0), lit(9)).cast(DataType::Int32) * lit(4)).alias("col_1m4"),
-        // (col("l").str().slice(lit(0), lit(9)).cast(DataType::Int32) * lit(10)).alias("col_1m10"),
-        col("l").str().slice(lit(9), lit(10)).alias("col_2"),
-        col("l").str().slice(lit(19), lit(6)).alias("col_3"),
-        col("l").str().slice(lit(25), lit(40)).alias("col_4"),
-        col("l").str().slice(lit(25), lit(1)).alias("col_5"),
-        col("l").str().slice(lit(26), lit(1)).alias("col_6"),
-        col("l").str().slice(lit(27), lit(1)).alias("col_7"),
-        col("l").str().slice(lit(28), lit(1)).alias("col_8"),
-        col("l").str().slice(lit(29), lit(1)).alias("col_9"),
-        col("l").str().slice(lit(30), lit(1)).alias("col_10"),
-        col("l").str().slice(lit(31), lit(1)).alias("col_11"),
-    ];
+    // let mut _vec_expr = [
+    //     col("l")
+    //         .str()
+    //         .slice(lit(0), lit(9))
+    //         .cast(DataType::Int32)
+    //         .alias("col_1"),
+    //     // (col("l").str().slice(lit(0), lit(9)).cast(DataType::Int32) * lit(4)).alias("col_1m4"),
+    //     // (col("l").str().slice(lit(0), lit(9)).cast(DataType::Int32) * lit(10)).alias("col_1m10"),
+    //     col("l").str().slice(lit(9), lit(10)).alias("col_2"),
+    //     col("l").str().slice(lit(19), lit(6)).alias("col_3"),
+    //     col("l").str().slice(lit(25), lit(40)).alias("col_4"),
+    //     col("l").str().slice(lit(25), lit(1)).alias("col_5"),
+    //     col("l").str().slice(lit(26), lit(1)).alias("col_6"),
+    //     col("l").str().slice(lit(27), lit(1)).alias("col_7"),
+    //     col("l").str().slice(lit(28), lit(1)).alias("col_8"),
+    //     col("l").str().slice(lit(29), lit(1)).alias("col_9"),
+    //     col("l").str().slice(lit(30), lit(1)).alias("col_10"),
+    //     col("l").str().slice(lit(31), lit(1)).alias("col_11"),
+    // ];
 
-    println!("{:?}", _vec_expr);
+    // println!("{:?}", _vec_expr);
 
     use std::time::Instant;
     let now = Instant::now();
@@ -39,8 +39,15 @@ pub fn main() {
         .finish()
         .unwrap();
 
+    println!("{:?}", data_.clone().collect());
+
     // append the polars lazyframe with the expressions generated above
-    let data_ = data_.with_columns(_vec_expr);
+    let data_ = data_
+        .with_columns([col("l")
+            .str()
+            .extract_groups(r"^(?<col_1>.{9})(?<col_2>.{10})(?<col_3>.{6})(?<col_4>.*)")
+            .unwrap()])
+        .unnest(["l"].into_iter());
 
     // collect
     println!("{:?}", data_.collect());
